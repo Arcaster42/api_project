48b31b6e09b3b47853de9dca1e74df7c2d07bb7f
TJ Epperson <tj.epperson070@gmail.com>
2019-07-22 10:43:12 +0900

    Added knex module

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const path = require('path')
 app.use('/', express.static(path.join(__dirname, '/')))
 
 //Modules
-
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'admin',
+        password: 'admin',
+        database: 'api_project'
+    }
+})
 
 //Routes
 app.use(require('./routes/root'))
