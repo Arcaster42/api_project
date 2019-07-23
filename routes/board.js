1f1a6e90f5a4e4463539d75d59d19647d4517eeb
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 17:49:27 +0900

    Retrieved posts via api

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const router = express()
-const knex = require('knex')
+const knex = require('../index')
+const db = knex.knex
 
 router.get('/board', (req, res) => {
-    knex('posts')
+    let posts;
+    let replies;
+    db('posts')
     .then((query) => {
-        res.render('board', {posts: query})
+        posts = query
+        db('replies')
+        .then((query) => {
+            replies = query
+        })
+        .then(() => {
+            res.render('board', {posts: posts, replies: replies})
+        })
+        .catch((err) => res.render(err))
     })
     .catch((err) => res.render(err))
 })
