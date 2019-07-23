178b474d9c354329b004de282052c820a9409cc5
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 21:26:14 +0900

    Added board route

--- routes/board.js
+++ routes/board.js
@@ -4,21 +4,7 @@ const knex = require('../index')
 const db = knex.knex
 
 router.get('/board', (req, res) => {
-    let posts;
-    let replies;
-    db('posts')
-    .then((query) => {
-        posts = query
-        db('replies')
-        .then((query) => {
-            replies = query
-        })
-        .then(() => {
-            res.render('board', {posts: posts, replies: replies})
-        })
-        .catch((err) => res.render(err))
-    })
-    .catch((err) => res.render(err))
+    res.render('board')
 })
 
 module.exports = router
