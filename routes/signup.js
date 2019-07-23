f77a842838b8af125982082994214794196da3b3
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 15:49:20 +0900

    Added password hashing

--- routes/signup.js
+++ routes/signup.js
@@ -4,6 +4,7 @@ const multer = require('multer')
 const upload = multer()
 const knex = require('../index')
 const db = knex.knex;
+const bcrypt = require('bcrypt')
 
 router.get('/signup', (req, res) => {
     res.render('signup')
@@ -11,21 +12,24 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup', upload.none(), (req, res) => {
     const username = req.body.username.toLowerCase()
+    const password = req.body.password
     db('users')
     .where({username: username})
     .then((query) => {
         if (query.length < 1) {
-            db('users')
-            .insert({
-                username: username,
-                email: 'test@email.com',
-                pw_hash: 'na',
-                can_put: true,
-                can_patch: true,
-                can_delete: true
+            bcrypt.hash(password, 10, (err, hash) => {
+                db('users')
+                .insert({
+                    username: username,
+                    email: 'test@email.com',
+                    pw_hash: hash,
+                    can_put: true,
+                    can_patch: true,
+                    can_delete: true
+                })
+                .then(() => res.send('Account created'))
+                .catch((err) => res.send(err))
             })
-            .then(() => res.send('Account created'))
-            .catch((err) => res.send(err))
         } else if (query.length > 0) {
             res.send('User already exists')
         }
