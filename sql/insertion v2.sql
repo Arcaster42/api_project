ed63fab5526c085bbefec78eb4e272f9e0f15246
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 17:23:45 +0900

    Scripted posts and replies

--- sql/insertion v2.sql
+++ sql/insertion v2.sql
@@ -1,5 +1,35 @@
+--Insert users
+INSERT INTO users
+VALUES
+('jsdev14', 'jsdev14@gmail.com', 'na', 'na', false, false, false),
+('supercat7', 'supercat7@gmail.com', 'na', 'na'),
+('randomcoder9', 'randomcoder9@gmail.com', 'na', 'na');
+
 --Insert topics
 INSERT INTO topics
 VALUES
 ('Variables'),
-('Functions');
+('Functions'),
+('HTML/CSS'),
+('Node/Express');
+
+--Insert posts
+INSERT INTO posts
+VALUES
+(DEFAULT, 'jsdev14', 'Node/Express', 'Setting up Express',
+'Does anyone know how to set up an express server in node?'),
+(DEFAULT, 'supercat7', 'HTML/CSS', 'CSS is Impossible',
+'Why is CSS impossible? You need like five different lines of code 
+just to center something. What a joke!'),
+(DEFAULT, 'randomcoder9', 'Variables', 'Why Boolean?',
+'Seriously, why is it called a boolean?');
+
+--Insert replies
+INSERT INTO replies
+VALUES
+(DEFAULT, 1, 'supercat7', 
+'You just need to run npm install express and then require express and 
+invoke the function! Or something like that. Google it.', '2019-07-23'),
+(DEFAULT, 1, 'randomcoder9',
+'Only horrible, terrible people even use npm. Use yarn you cretin.', 
+'2019-07-23');
