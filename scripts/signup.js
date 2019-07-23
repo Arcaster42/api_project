db1b09080315f09552ae02998f13ea69538b85dd
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 16:23:24 +0900

    Added form submission check

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,7 +1,19 @@
+let valid = false;
+
 function checkPass() {
     const p1 = document.getElementById('p1')
     const p2 = document.getElementById('p2')
     const lbl = document.getElementById('lbl')
-    if (p1.value === p2.value) lbl.textContent = 'Password'
-    if (p1.value !== p2.value) lbl.textContent = 'Password (Must Match)'
+    if (p1.value === p2.value && p1.value.length > 5) {
+        lbl.textContent = 'Password'
+        valid = true;
+    }
+    if (p1.value !== p2.value) {
+        lbl.textContent = 'Password (Must Match)'
+        valid = false;
+    }
+}
+
+function isValid() {
+    return valid;
 }
