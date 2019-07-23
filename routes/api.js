c354f5d6e8a5557c3169fd50bb87f632d3bc2a02
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 14:07:42 +0900

    Implemented get for roles

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express()
 const knex = require('../index.js')
+const {Model} = require('../index.js')
 
 router.get('/api/roles', (req, res) => {
-    let query = knex.select().from('roles')
-    console.log(query)
-    res.send(query)
+    let result;
+    knex('roles')
+    .then((results) => {
+        result = JSON.stringify(results)
+        res.send(result)
+    })
 })
 
 module.exports = router
