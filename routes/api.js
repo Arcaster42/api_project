e10418222ee57dc5c1dec7a3658268d10a73a855
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 21:25:43 +0900

    Added replies endpoint

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,17 @@ const knex = require('../index.js')
 const db = knex.knex
 
 router.get('/api/posts', (req, res) => {
-    let result;
+    let result
     db('posts')
+    .then((results) => {
+        result = JSON.stringify(results)
+        res.send(result)
+    })
+})
+
+router.get('/api/replies', (req, res) => {
+    let result
+    db('replies')
     .then((results) => {
         result = JSON.stringify(results)
         res.send(result)
