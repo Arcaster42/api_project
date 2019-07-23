1f45b4e928af14824dfe2003cf2c72dd971132fc
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 09:52:00 +0900

    Added jobs

--- sql/insertion.sql
+++ sql/insertion.sql
@@ -1,5 +1,27 @@
+--Insert role values
 INSERT INTO roles
 VALUES
 (DEFAULT, 'Tank', 'Aggro control'),
 (DEFAULT, 'Healer', 'Damage mitigation'),
 (DEFAULT, 'Damage', 'Damage output');
+
+--Insert job values
+INSERT INTO jobs
+VALUES
+(DEFAULT, 'Paladin', 1, 'Melee', 'Strong mitigation, self-healing, high utility'),
+(DEFAULT, 'Warrior', 1, 'Melee', 'Strong mitigation, high damage'),
+(DEFAULT, 'Dark Knight', 'Melee', 1, 'Highest mitigation, excellent weaving'),
+(DEFAULT, 'Gunbreaker', 'Melee', 1, 'High utility, good weaving, good burst'),
+(DEFAULT, 'White Mage', 'Ranged', 2, 'High single-target, good AoE, high damage'),
+(DEFAULT, 'Scholar', 'Ranged', 2, 'Good utility, shielding, good weaving'),
+(DEFAULT, 'Astrologian', 'Ranged', 2, 'Great utility, shielding or regen, good burst'),
+(DEFAULT, 'Dragoon', 'Melee', 3, 'High damage, high mobility, good sustain'),
+(DEFAULT, 'Monk', 'Melee', 3, 'Good damage, flexible combos, moderate sustain'),
+(DEFAULT, 'Ninja', 'Melee', 3, 'High damage, lowest global cooldown, high sustain'),
+(DEFAULT, 'Samurai', 'Melee', 3, 'Extreme damage, simple combos'),
+(DEFAULT, 'Bard', 'Ranged', 3, 'High utility, high damage'),
+(DEFAULT, 'Machinist', 'Ranged', 3, 'Revamped'),
+(DEFAULT, 'Dancer', 'Ranged', 3, 'High damage, intuitive combos, dynamic combos'),
+(DEFAULT, 'Black Mage', 'Ranged', 3, 'Extreme damage, low mobility, moderate sustain'),
+(DEFAULT, 'Summoner', 'Ranged', 3, 'High damage, extreme burst, good utiltiy'),
+(DEFAULT, 'Red Mage', 'Ranged', 3, 'High damage, intuitive combos, good utility');
