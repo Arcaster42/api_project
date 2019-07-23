5a4bf3c26d0099f3f97962c0470e1dd681b07cad
TJ Epperson <tj.epperson070@gmail.com>
2019-07-23 12:18:56 +0900

    Added warrior and partial dark knight skills

--- sql/insertion.sql
+++ sql/insertion.sql
@@ -29,10 +29,25 @@ VALUES
 --Insert skills
 INSERT INTO skills
 VALUES
+--Paladin
 (DEFAULT, 'Fast Blade', 'Melee', 'Physical Attack', 'Combo starter', NULL, NULL),
 (DEFAULT, 'Riot Blade', 'Melee', 'Physical Attack', 'Combo continuation', NULL, NULL),
 (DEFAULT, 'Royal Authority', 'Melee', 'Physical Attack', 'Combo finisher', NULL, NULL),
+(DEFAULT, 'Goring Blade', 'Melee', 'Physical DoT', 'Combo finisher', NULL, NULL),
 (DEFAULT, 'Total Eclipse', 'Melee AoE', 'Physical Attack', 'Combo starter', NULL, NULL),
 (DEFAULT, 'Prominence', 'Melee AoE', 'Physical Attack', 'Combo finisher', NULL, NULL),
 (DEFAULT, 'Fight of Flight', 'Damage Increase', 'Buff', '25% damage increase', NULL, NULL),
-(DEFAULT, 'Sheltron', 'Damage Mitigation', 'Buff', '6-second all-block', NULL, NULL);
+(DEFAULT, 'Sheltron', 'Damage Mitigation', 'Buff', '6-second all-block', NULL, NULL),
+--Warrior
+(DEFAULT, 'Heavy Swing', 'Melee', 'Physical Attack', 'Combo starter', NULL, NULL),
+(DEFAULT, 'Maim', 'Melee', 'Physical Attack', 'Combo continuation', NULL, NULL),
+(DEFAULT, 'Storms Path', 'Melee', 'Physical Attack', 'Combo fininsher', NULL, NULL),
+(DEFAULT, 'Storms Eye', 'Melee', 'Physical Attack', 'Combo finisher', NULL, NULL),
+(DEFAULT, 'Overpower', 'Melee AoE', 'Physical Attack', 'Combo starter', NULL, NULL),
+(DEFAULT, 'Mythril Tempest', 'Melee AoE', 'Physical Attack', 'Combo finisher', NULL, NULL),
+(DEFAULT, 'Berserk', 'Damage Increase', 'Buff', 'All attacks critically hit', NULL, NULL),
+(DEFAULT, 'Thrill of Battle', 'Damage Mitigation', 'Buff', '20% HP increase and heal', NULL, NULL),
+--Dark Knight
+(DEFAULT, 'Hard Slash', 'Melee', 'Physical Attack', 'Combo starter', NULL, NULL),
+(DEFAULT, 'Syphon Strike', 'Melee', 'Physical Attack', 'Combo continuation', NULL, NULL),
+(DEFAULT, 'Souleater', 'Melee', 'Physical Attack', 'Combo finisher', NULL, NULL);
