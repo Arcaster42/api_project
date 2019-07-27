fdfad002a40c2f7fab6d891431eb9a40a1274aa4
TJ Epperson <tj.epperson070@gmail.com>
2019-07-27 21:09:12 +0900

    Fixed column names

--- routes/reply.js
+++ routes/reply.js
@@ -8,17 +8,17 @@ const moment = require('moment')
 
 router.post('/reply', upload.none(), (req, res) => {
     const parent = req.body.parent_hid
-    const author = req.body.author_hid
+    const author = req.session.user
     const content = req.body.content
     const date = moment().format('YYYY-MM-DD')
     const time = moment().format('HH:MM A')
     db('replies')
     .insert({
         parent: parent,
-        author: 'jsdev14',
+        author: author,
         content: content,
-        date: date,
-        time: time})
+        date_stamp: date,
+        time_stamp: time})
     .then(() => {
         console.log('inserted into DB')
     })
