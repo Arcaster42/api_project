983dc39147d6bf019bac9c43bb61cb810a91ee11
TJ Epperson <tj.epperson070@gmail.com>
2019-07-27 11:13:35 +0900

    Added for migration/testing

---
+++ sql/migration.sql
@@ -0,0 +1,45 @@
+--Create DB
+CREATE DATABASE api_project;
+
+--Create users table
+CREATE TABLE users (
+    username VARCHAR PRIMARY KEY,
+    email VARCHAR NOT NULL,
+    pw_hash VARCHAR NOT NULL,
+    api_key TEXT,
+    can_put BOOLEAN,
+    can_patch BOOLEAN,
+    can_delete BOOLEAN
+);
+
+--Create api_keys table
+CREATE TABLE api_keys (
+    api_key VARCHAR PRIMARY KEY,
+    is_admin BOOLEAN
+)
+
+--Create topics table
+CREATE TABLE topics (
+    topic VARCHAR PRIMARY KEY
+);
+
+--Create posts table
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    author VARCHAR REFERENCES users(username),
+    topic VARCHAR REFERENCES topics(topic),
+    title VARCHAR NOT NULL,
+    content TEXT NOT NULL,
+    date_stamp VARCHAR,
+    time_stamp VARCHAR
+);
+
+--Create replies table
+CREATE TABLE replies (
+    id SERIAL PRIMARY KEY,
+    parent INTEGER REFERENCES posts(id),
+    author VARCHAR REFERENCES users(username),
+    content TEXT NOT NULL,
+    date_stamp VARCHAR,
+    time_stamp VARCHAR
+);
---
+++ sql/seed.sql
@@ -0,0 +1,47 @@
+--Insert users
+INSERT INTO users
+VALUES
+('admin42', 'admin42@gmail.com', 'na', 'abcdefg123'),
+('jsdev14', 'jsdev14@gmail.com', 'na'),
+('supercat7', 'supercat7@gmail.com', 'na'),
+('randomcoder9', 'randomcoder9@gmail.com', 'na'),
+('danishbagel21', 'danishbagel21@gmail.com', 'na');
+
+--Insert topics
+INSERT INTO topics
+VALUES
+('Variables'),
+('Functions'),
+('HTML/CSS'),
+('Node/Express'),
+('Database'),
+('API');
+
+--Insert posts
+INSERT INTO posts
+VALUES
+(DEFAULT, 'jsdev14', 'Node/Express', 'Setting up Express',
+'Does anyone know how to set up an express server in node?', 
+'2019-07-22', '8:25 AM'),
+(DEFAULT, 'supercat7', 'HTML/CSS', 'CSS is Impossible',
+'Why is CSS impossible? You need like five different lines of code 
+just to center something. What a joke!', 
+'2019-07-22', '9:45 PM'),
+(DEFAULT, 'randomcoder9', 'Variables', 'Why Boolean?',
+'Seriously, why is it called a boolean?', 
+'2019-07-23', '7:32 AM');
+
+--Insert replies
+INSERT INTO replies
+VALUES
+(DEFAULT, 1, 'supercat7', 
+'You just need to run npm install express and then require express and 
+invoke the function! Or something like that. Google it.', 
+'2019-07-23', '9:43 AM'),
+(DEFAULT, 1, 'randomcoder9',
+'Only horrible, terrible people even use npm. Use yarn you cretin.', 
+'2019-07-23', '11:52 AM'),
+(DEFAULT, 2, 'jsdev14',
+'CSS was made by the CIA in an experiment to see how much punishment 
+a single human would bring upon themself. Back-end for life!', 
+'2019-07-24', '1:32 PM');
