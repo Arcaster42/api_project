6a4db14cfd21e7884aad662363bf9026971a0bf1
TJ Epperson <tj.epperson070@gmail.com>
2019-07-28 13:11:52 +0900

    Minor adjustments

--- test/spec.js
+++ test/spec.js
@@ -2,4 +2,27 @@ const mocha = require('mocha')
 const chai = require('chai')
 const should = chai.should()
 const expect = chai.expect
+const server = require('../index')
+const http = require('http')
 
+
+describe('API Project', () => {
+    describe('Server', () => {
+        before(() => {
+            
+        })
+        after(() => {
+            server.app.close()
+        })
+
+        it('should return something', (done) => {
+            http.get('http://localhost:3000/', (res) => {
+                expect(res.statusCode).to.eql(200)
+            })
+        })
+    })
+    
+    it('should exists', () => {
+        expect(true).to.eql(true)
+    })
+})
